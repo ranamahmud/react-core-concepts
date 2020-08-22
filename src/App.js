00196055aaddf7b63224c9887ba38acb0e0e5c88
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-23 03:05:05 +0600

    JSX, Dynamic content, Dynamic Style done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dr. Mahfuz",
+    job: "Singer"
+  };
+
+  var person2 = {
+    name: "Eva Rahman",
+    job: "Kokil Kanthi"
+  }
+  var style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,10 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="" style={style}>My heading: {2 + 5 * 25 + 65}</h1>
+        <h2>Heading: {person.name + " "+person.job}</h2>
+        <h3 style = {{backgroundColor:'cyan',color:'yellow'}}>Singer: {person2.name + " "+person2.name}</h3>
+        <p>My first React Paragraph</p>
       </header>
     </div>
   );
